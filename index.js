80f2ac11318afae5a2dcdbf7b2dd05378b515b91
Fran Borg <francesca.bte@outlook.com>
2019-10-10 11:13:05 +0200

    Add starting file

---
+++ index.js
@@ -0,0 +1,10 @@
+// Answer Sheet - all answers should go here 
+
+
+
+
+
+
+
+// Do not edit or answer below this line
+module.exports = {profileData, convertObjectToArray, convertArrayData, getNestedData, wordConverter, calculateHours, Course, capitalizeFirstLetter, validPin};
