6c568a5f851e278f8a578ed0b9b6625695350922
Fran Borg <francesca.bte@outlook.com>
2019-11-11 11:47:07 +0100

    Format output for clarity

--- testing/test.js
+++ testing/test.js
@@ -94,8 +94,8 @@ describe('Course details should', () => {
 describe('Course spaceNeeded should', () => {
     test.each`
     teacher | type | number | expected 
-    ${"John"} | ${"web development"} | ${22} | ${"The class should be 44m²."}  
-    ${"Jane"} | ${"marketing"} | ${19} | ${"The class should be 38m²."} 
+    ${"John"} | ${"web development"} | ${22} | ${"The classroom should be 44m²."}  
+    ${"Jane"} | ${"marketing"} | ${19} | ${"The classroom should be 38m²."} 
     `('return the description: $expected', ({ teacher, type, number, expected }) => {
         expect(new Course(teacher, type, number).spaceNeeded()).toStrictEqual(expected);
     });
